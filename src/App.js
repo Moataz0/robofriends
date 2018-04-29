3736746607a3a227b2d74bafeefef798ec254c20
Igor Perun <iperun23@gmail.com>
2018-04-29 16:00:02 -0400

    Application main content file added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CardList from './CardList.js';
+import SearchBox from './SearchBox.js';
+import {robots} from './robots.js';
+
+
+
+const App = () => {
+    return (
+        <div className='tc'>
+            <h1>RoboFriends</h1>
+            <SearchBox />
+            <CardList robots={robots} />
+        </div>
+        
+    );
+}
+
+export default App;
