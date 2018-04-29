9efcbf54e98b27244919236253f63bbfebd874fe
Igor Perun <iperun23@gmail.com>
2018-04-29 14:50:49 -0400

    added data array for robots

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Robot 1',
+        username: 'Robo1',
+        email: 'robo1@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Robot 2',
+        username: 'Robo2',
+        email: 'robo2@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Robot 3',
+        username: 'Robo3',
+        email: 'robo3@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Robot 4',
+        username: 'Robo4',
+        email: 'robo4@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Robot 5',
+        username: 'Robo5',
+        email: 'robo5@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Robot 6',
+        username: 'Robo6',
+        email: 'robo6@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Robot 7',
+        username: 'Robo7',
+        email: 'robo7@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Robot 8',
+        username: 'Robo8',
+        email: 'robo8@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Robot 9',
+        username: 'Robo9',
+        email: 'robo9@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Robot 10',
+        username: 'Robo10',
+        email: 'robo10@gmail.com'
+    }
+];
